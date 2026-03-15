6fb9b2b49f9217905ad2d79a30f151293a16cfca
Azul Montalvo <montalvoazul5@gmail.com>
2026-03-15 14:34:22 -0600

    perf: mejoras en el navbar. Estilos para el hero.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,23 @@
 function Hero() {
   return (
     <section className="hero">
+      <section className="hero-content">
       <h1>Hola, soy Azul</h1>
-      <p>Desarrollador Frontend</p>
+      <p>
+        Desarrolladora Frontend enfocada en crear experiencias digitales
+        fluidas, intuitivas y de alto rendimiento. Transformo ideas complejas en
+        interfaces limpias y funcionales.
+      </p>
+      <button id="btnProyectos" className="hero-button">Ver proyectos</button>
+      <button id="btnContactar" className="hero-button">Contactar</button>
+      </section>
+      <img
+        src="src/assets/images/retrato-ilustracion.png"
+        alt="Hero Image"
+        className="hero-image"
+      />
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,22 @@
-import { HomeIcon, AboutIcon, SkillsIcon, ProjectsIcon, ExperienceIcon, ContactIcon } from "../assets/icons/Icons"
-
 function Navbar() {
-    const title = "<am/>"
-    const navLinks = [
-        // { name: "Home", href: "#home", icon: <HomeIcon /> },
-        { name: "Sobre mí", href: "#about", icon: <AboutIcon /> },
-        { name: "Skills", href: "#skills", icon: <SkillsIcon /> },
-        { name: "Proyectos", href: "#projects", icon: <ProjectsIcon /> },
-        { name: "Experiencia", href: "#experience", icon: <ExperienceIcon /> },
-        { name: "Contáctame", href: "#contact", icon: <ContactIcon /> },
-    ]
-
   return (
     <header className="navbar">
         <div className="nav-container">
             <h1 className="nav-title"><a href="#home">Azul Montalvo</a></h1>
             <nav>
                 <ul className="nav-links">
-                    {navLinks.map((link) => (
-                        <li key={link.name} className="nav-item">
-                            <a href={link.href} className="nav-link">
-                                <span className="nav-icon">{link.icon}</span>
-                                <span className="nav-text">{link.name}</span>
-                            </a>
-                        </li>
-                    ))}
+                    <li className="nav-item">
+                        <a href="#about" className="nav-link nav-text">Sobre mí</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#skills" className="nav-link nav-text">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#projects" className="nav-link nav-text">Proyectos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#experience" className="nav-link nav-text">Experiencia</a>
+                    </li>
                 </ul>
             </nav>
         </div>
