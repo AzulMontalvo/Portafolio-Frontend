91444c4735a5c17515526d49ba016ffd7e217cad
Azul Montalvo <montalvoazul5@gmail.com>
2026-04-30 01:37:49 -0600

    feat: copy y estilos de hero y about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,26 @@
-import heroImage from '../assets/images/retrato-ilustracion.png';
+import heroImage from "../assets/images/retrato-ilustracion.png";
 
 function Hero() {
   return (
     <section className="hero">
       <section className="hero-content">
-      <h1>Hola, soy Azul</h1>
-      <p>
-        Desarrolladora Frontend enfocada en crear experiencias digitales
-        fluidas, intuitivas y de alto rendimiento. Transformo ideas complejas en
-        interfaces limpias y funcionales.
-      </p>
-      <button id="btnProyectos" className="hero-button">Ver proyectos</button>
-      <button id="btnContactar" className="hero-button">Contactar</button>
+        <h1 className="section-title">
+          ¡Hola! <br></br> Soy Azul Montalvo
+        </h1>
+        <p>
+          Desarrolladora Full Stack especializada en crear aplicaciones web
+          completas, combinando interfaces de usuario fluidas con arquitecturas
+          de backend robustas y escalables. Transformo ideas complejas en
+          soluciones digitales integrales y eficientes.
+        </p>
+        <a id="btnProyectos" className="btn-base" href='#projects'>
+          Ver proyectos
+        </a>
+        <a id="btnContactar" className="btn-base btn-light" href='#contact'>
+          Contactar
+        </a>
       </section>
-      <img
-        src={heroImage}
-        alt="Hero Image"
-        className="hero-image"
-      />
+      <img src={heroImage} alt="Hero Image" className="hero-image" />
     </section>
   );
 }
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,10 +1,47 @@
+import { DownloadIcon } from "../assets/icons/Icons";
+
 function About() {
   return (
-    <section className="about">
-      <h2>Sobre mí</h2>
-      <p>Soy un desarrollador frontend con experiencia en React, JavaScript y CSS. Me apasiona crear interfaces de usuario atractivas y funcionales.</p>
+    <section id="about" className="about">
+      <div className="about-glow"></div>
+      <h2 className="section-title">Sobre mí</h2>
+      <section>
+        <h3 className="about-quote">
+          Soluciones integrales, desde la lógica del servidor hasta la
+          interacción del usuario.
+        </h3>
+        <section className="stats-container">
+          <div className="stat-item">
+            <span>+2</span> años de exp.
+          </div>
+          <div className="stat-item">
+            <span>10+</span> Proyectos
+          </div>
+          <div className="stat-item">
+            <span>100%</span> Escalable
+          </div>
+        </section>
+      </section>
+      <article className="about-right">
+        <p>
+          Soy <strong>Ingeniera en Sistemas</strong> con una especialización marcada en el
+          desarrollo de <strong>aplicaciones integrales</strong>. Mi formación me permite abordar
+          cada proyecto desde una estructura lógica y técnica sólida,
+          garantizando que tanto la experiencia del usuario como la arquitectura
+          del servidor estén respaldadas por un código <strong>eficiente</strong> y <strong>escalable</strong>.
+        </p>
+        <p>
+          Mi objetivo es construir puentes entre las necesidades del negocio y
+          soluciones tecnológicas completas, transformando requerimientos
+          complejos en ecosistemas digitales fluidos. Me apasiona el reto de
+          optimizar procesos de extremo a extremo y cuidar el equilibrio entre
+          un <strong>backend robusto</strong> y un <strong>frontend impecable</strong> para crear productos que
+          realmente aporten valor.
+        </p>
+      </article>
+      <a href="public/CV.pdf" download id="btnDescargarCV" className="btn-base btn-dark btn-cv">Descargar CV <DownloadIcon /></a>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
