2e94e4ef5d8c0b84dd4997effe4160f6b633a03b
Azul Montalvo <montalvoazul5@gmail.com>
2026-05-26 17:44:56 -0600

    feat: sección footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+      <div className="footer-socials">
+        <a
+          href="https://github.com/AzulMontalvo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-card-link"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://linkedin.com/in/azul-montalvo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-card-link"
+        >
+          LinkedIn
+        </a>
+      </div>
+      <p>
+        &copy; {new Date().getFullYear()} Azul. Todos los derechos reservados.
+      </p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
